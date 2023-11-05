2dc135a848ae2ca570147981578804af9c48778c
Dimitris <34748845+DimitrisChoul@users.noreply.github.com>
2023-11-05 08:15:57 +0000

    Update 3 files

--- myApp.js
+++ myApp.js
@@ -1,39 +1,6 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+console.log("Hello World");
+app.get("/", function (req, res) {
+  res.send("Hello Express");
+});
